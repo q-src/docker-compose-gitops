6146322f5c26cdd3460152c3b3b346d544492994
Adrian Pauli <pauli.adrian96@gmail.com>
2020-04-13 20:42:30 +0200

    Add workdir setting

--- dockercompose/compose.go
+++ dockercompose/compose.go
@@ -1,11 +1,14 @@
 package dockercompose
 
 import (
+	"fmt"
 	"github.com/op/go-logging"
+	"github.com/q-src/envconf/envconf"
 	"os"
 	"os/exec"
 )
 
+var workdir = envconf.Env{Name: "WORKDIR", DefaultValue: ""}.Get()
 var defaultArgs = []string{"up", "-d"}
 var log = logging.MustGetLogger("gitops")
 
@@ -29,9 +32,10 @@ func Run(path string, customArgs []string) error {
 func createCommand(path string, subcommand string, subargs ...string) *exec.Cmd {
 	args := append([]string{subcommand}, subargs...)
 	cmd := exec.Command("docker-compose", args...)
-	cmd.Dir = path
+	cmd.Dir = fmt.Sprintf("%s/%s", path, workdir)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	log.Debugf("Running docker-compose %s with args '%s'",subcommand, subargs)
+	log.Debugf("Running docker-compose %s with args '%s'", subcommand, subargs)
+	log.Debugf("Selected workdir is '%s'.", cmd.Dir)
 	return cmd
 }

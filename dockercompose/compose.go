30fd9c066616cc2bc103e57f9d35b914d89829c4
Adrian Pauli <pauli.adrian96@gmail.com>
2020-04-13 16:44:21 +0200

    Ensure images are pulled before service start

--- dockercompose/compose.go
+++ dockercompose/compose.go
@@ -11,14 +11,27 @@ var log = logging.MustGetLogger("gitops")
 
 func Run(path string, customArgs []string) error {
 	args := append(defaultArgs, customArgs...)
-	cmd := exec.Command("docker-compose", args...)
-	cmd.Dir = path
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	log.Debugf("Running docker-compose with args '%s'", args)
+
+	cmd := createCommand(path, "pull")
 	err := cmd.Run()
+	if nil != err {
+		log.Warningf("docker-compose pull failed: '%s'", err)
+	}
+
+	cmd = createCommand(path, "up", args...)
+	err = cmd.Run()
 	if nil != err {
 		log.Errorf("docker-compose failed: '%s'", err)
 	}
 	return err
+}
+
+func createCommand(path string, subcommand string, subargs ...string) *exec.Cmd {
+	args := append([]string{subcommand}, subargs...)
+	cmd := exec.Command("docker-compose", args...)
+	cmd.Dir = path
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	log.Debugf("Running docker-compose %s with args '%s'",subcommand, subargs)
+	return cmd
 }
